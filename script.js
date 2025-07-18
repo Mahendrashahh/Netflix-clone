1bfffe564a21ad313b2400f72bd8234ac8e74270
Mahendrashahh <shahmahendra380@gmail.com>
2025-07-18 16:27:05 +0530

    learning purpose

---
+++ script.js
@@ -0,0 +1,10 @@
+function slideLeft() {
+  const container = document.getElementById("cardContainer");
+  container.scrollLeft -= 320;
+}
+
+function slideRight() {
+  const container = document.getElementById("cardContainer");
+  container.scrollLeft += 320;
+}
+
